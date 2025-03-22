df2e18009c16fe301ac8bbaf935b3e793239918e
Rene Schallner <rene@zml.ai>
2025-03-22 08:31:36 +0100

    get rid of Args parameter in flagz.deinit()

--- src/flagz.zig
+++ src/flagz.zig
@@ -142,8 +142,8 @@ pub fn parse(comptime T: type, allocator: std.mem.Allocator) !T {
     return result;
 }
 
-pub fn deinit(comptime T: type, args: T, allocator: std.mem.Allocator) void {
-    const fields = std.meta.fields(T);
+pub fn deinit(args: anytype, allocator: std.mem.Allocator) void {
+    const fields = std.meta.fields(@TypeOf(args));
     inline for (fields) |field| {
         if (@typeInfo(field.type) == .pointer and (field.type == []u8 or field.type == []const u8)) {
             if (@field(args, field.name).len > 0) {

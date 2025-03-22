8ec96e2de8087207d808746d8f039dd96394aef5
Rene Schallner <rene@zml.ai>
2025-03-22 08:06:24 +0100

    add test step to build.zig

--- build.zig
+++ build.zig
@@ -28,4 +28,11 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the example");
     run_step.dependOn(&run_cmd.step);
+
+    // Tests
+    const tests = b.addTest(.{ .root_source_file = b.path("src/test.zig") });
+    // tests.root_module.addImport("flagz", flagz_module);
+    const run_tests = b.addRunArtifact(tests);
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_tests.step);
 }
